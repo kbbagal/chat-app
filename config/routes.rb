cda4ff7f15ff54d4da5df1b8a93e2c6aeef48022
= <=>
2020-04-25 16:01:03 +0530

    Creating login front end, user and messages resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'chatrooms#index'
+  root 'sessions#login'
   get 'login', to: 'sessions#login'
   get 'logout', to: 'sessions#logout'
   post 'create', to: 'sessions#create'
+  get 'chatroom', to: 'chatrooms#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: 'kishor', password: '123')
+User.create(username: 'rupali', password: '123')
+User.create(username: 'tushar', password: '123')
+User.create(username: 'shmbbhu', password: '123')
